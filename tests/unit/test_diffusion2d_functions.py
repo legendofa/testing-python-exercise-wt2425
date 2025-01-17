64d1b2cf90c1de25620288752fe630cbf59aff26
st170001 <st170001@stud.uni-stuttgart.de>
2025-01-17 19:05:33 +0100

    Refined unittests with tolerance for array comparisons

--- tests/unit/test_diffusion2d_functions.py
+++ tests/unit/test_diffusion2d_functions.py
@@ -7,7 +7,7 @@ from diffusion2d import SolveDiffusion2D
 import numpy
 import sys
 
-# Append parent directory path to allow SolveDiffusion2D import.
+# Append parent directory path to system paths to allow SolveDiffusion2D import.
 sys.path.append('../..')
 
 numpy.set_printoptions(threshold=sys.maxsize)
@@ -26,8 +26,8 @@ class TestDiffusion2D(unittest.TestCase):
         dx = 0.5
         dy = 0.5
         self.solver.initialize_domain(w, h, dx, dy)
-        self.assertAlmostEqual(self.solver.nx, 40, places=2)
-        self.assertAlmostEqual(self.solver.ny, 60, places=2)
+        self.assertAlmostEqual(self.solver.nx, 40, places=3)
+        self.assertAlmostEqual(self.solver.ny, 60, places=3)
         self.assertEqual(self.solver.dx, dx)
         self.assertEqual(self.solver.dy, dy)
 
@@ -45,10 +45,10 @@ class TestDiffusion2D(unittest.TestCase):
         self.solver.nx = 40.
         self.solver.ny = 60.
         self.solver.initialize_physical_parameters(d, T_cold, T_hot)
-        self.assertAlmostEqual(self.solver.D, d, places=2)
-        self.assertAlmostEqual(self.solver.T_cold, T_cold, places=2)
-        self.assertAlmostEqual(self.solver.T_hot, T_hot, places=2)
-        self.assertAlmostEqual(self.solver.dt, 0.015, places=2)
+        self.assertAlmostEqual(self.solver.D, d, places=3)
+        self.assertAlmostEqual(self.solver.T_cold, T_cold, places=3)
+        self.assertAlmostEqual(self.solver.T_hot, T_hot, places=3)
+        self.assertAlmostEqual(self.solver.dt, 0.016, places=3)
 
     def test_set_initial_condition(self):
         """
@@ -66,15 +66,18 @@ class TestDiffusion2D(unittest.TestCase):
         self.solver.dt = 0.015625
         u = self.solver.set_initial_condition()
         self.assertEqual(u.shape, (4, 6))
-        self.assertTrue(numpy.array_equal(u,
-            numpy.array(
+        # numpy.allclose allows an absolute tolerance of 1e-03, 3 places in this configuration.
+        self.assertTrue(numpy.allclose(a=u,
+            b=numpy.array(
                 [
                     [300., 300., 300., 300., 300., 300.],
                     [300., 300., 300., 300., 300., 300.],
                     [300., 300., 300., 300., 300., 300.],
                     [300., 300., 300., 300., 300., 300.]
                 ]
-            )
+            ),
+            atol=1e-03,
+            rtol=0
         ))
 
 if __name__ == '__main__':
